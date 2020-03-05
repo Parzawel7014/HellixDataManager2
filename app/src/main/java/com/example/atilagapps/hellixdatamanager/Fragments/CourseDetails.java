62a7fc7fd453987395d07e50080550eec4639ab8
sangal.sahare <sangal7014@gmail.com>
2020-03-05 14:54:37 +0530

    initial commit

--- app/src/main/java/com/example/atilagapps/hellixdatamanager/Fragments/CourseDetails.java
+++ app/src/main/java/com/example/atilagapps/hellixdatamanager/Fragments/CourseDetails.java
@@ -1,25 +1,47 @@
 package com.example.atilagapps.hellixdatamanager.Fragments;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.atilagapps.hellixdatamanager.R;
 
 
-public class CourseDetails extends Fragment {
+public class CourseDetails extends Fragment implements TimePickerDialog.OnTimeSetListener {
 
+    Button timeButton;
+    TextView FromTxtView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View v=inflater.inflate(R.layout.fragment_course_details,container,false);
 
+        timeButton=v.findViewById(R.id.FromTimeId);
+        FromTxtView=v.findViewById(R.id.FromTextViewId);
+
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timepicker=new TimePickerFragment();
+                timepicker.show(getFragmentManager(),"time picker");
+            }
+        });
 
         return v;
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        FromTxtView.setText(hourOfDay+minute);
     }
 }
