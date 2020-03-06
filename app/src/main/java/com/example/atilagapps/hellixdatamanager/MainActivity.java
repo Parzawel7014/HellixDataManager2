532841fa12f1649b0a30afa484709e204c9e7aec
sangal.sahare <sangal7014@gmail.com>
2020-03-06 15:01:00 +0530

    2nd Commit

--- app/src/main/java/com/example/atilagapps/hellixdatamanager/MainActivity.java
+++ app/src/main/java/com/example/atilagapps/hellixdatamanager/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.atilagapps.hellixdatamanager;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -9,36 +13,88 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+//import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 
+import com.example.atilagapps.hellixdatamanager.Fragments.CourseDetails;
 import com.example.atilagapps.hellixdatamanager.Fragments.PersonalInfo_Fragment;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
     DataBaseHelper dbRef;
     Button nextButton;
-
+    private DrawerLayout drawerLayout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbRef=new DataBaseHelper(this);
+        dbRef = new DataBaseHelper(this);
 
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 
-        fragmentTransaction.add(R.id.containerId, new PersonalInfo_Fragment());
-        fragmentTransaction.commit();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        if (savedInstanceState == null) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+            fragmentTransaction.add(R.id.containerId, new PersonalInfo_Fragment());
+            fragmentTransaction.commit();
+            navigationView.setCheckedItem(R.id.nav_homeId);
+        }
 
     }
 
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
 
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_homeId:
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 
+                fragmentTransaction.add(R.id.containerId, new PersonalInfo_Fragment());
+                fragmentTransaction.commit();
+
+                break;
+            case R.id.nav_Add_Student_Id:
+
+                FragmentTransaction fm = getSupportFragmentManager().beginTransaction();
+                fm.replace(R.id.containerId, new CourseDetails()).addToBackStack(null).commit();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
     /*private void ShowData() {
 
